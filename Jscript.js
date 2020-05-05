e20183e631db12c10669bbf9cc6892ddf831b500
adarrak <59066830+adarrak@users.noreply.github.com>
2020-05-05 23:55:33 +0300

    add radio_list, radio_map

--- Jscript.js
+++ Jscript.js
@@ -4,11 +4,18 @@ document.querySelector('.burger_768').onclick=function(){
 };
 let polis=document.querySelector('.polis');
 let institut=document.querySelector('.institut');
+let polis_selected=document.querySelector('.polis_selected');
+let institut_selected=document.querySelector('.institut_selected');
 polis.onclick=function(){
 	polis.classList.add('selected');
 	institut.classList.remove('selected')
+	institut_selected.classList.add('disabled');
+	polis_selected.classList.remove('disabled');
 };
 institut.onclick=function(){
 	institut.classList.add('selected');
-	polis.classList.remove('selected')
+	polis.classList.remove('selected');
+	polis_selected.classList.add('disabled');
+	institut_selected.classList.remove('disabled');
+
 };

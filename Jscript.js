4f9ce0691fc4848daa37ec2b3cb8d2058de70af0
adarrak <59066830+adarrak@users.noreply.github.com>
2020-05-09 00:09:07 +0300

    add map_list

--- Jscript.js
+++ Jscript.js
@@ -19,3 +19,24 @@ institut.onclick=function(){
 	institut_selected.classList.remove('disabled');
 };
 
+let checkList=document.getElementById('list');
+let map=document.getElementById('map');
+let alp=document.querySelector('.list_left_alpha');
+let bet=document.querySelector('.list_left_betta');
+let filtr=document.querySelector('.filter');
+let imgMap=document.querySelector('.img_map');
+
+checkList.onclick=function(){
+	alp.classList.remove('disabled');
+	bet.classList.add('disabled');
+	filtr.classList.remove('disabled');
+	imgMap.classList.remove('disabled');
+}
+
+map.onclick=function(){
+	alp.classList.add('disabled');
+	bet.classList.remove('disabled');
+	filtr.classList.add('disabled');
+	imgMap.classList.add('disabled');
+
+}
